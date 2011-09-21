c155e9a52e22cf40da030729de32aa995c754b3f
matheus.nani@gmail.com <matheus.nani@gmail.com@3a0a1009-d4e9-2667-f0a4-3e9fb4aa316e>
2011-09-21 08:28:31 +0000

    modificacoes botoes redes sociais

--- LasTiras/LasTiras-web/src/main/java/br/com/lastiras/LasTirasList.java
+++ LasTiras/LasTiras-web/src/main/java/br/com/lastiras/LasTirasList.java
@@ -11,6 +11,7 @@ import br.com.lastiras.persistence.LasTirasStrip;
 import br.com.lastiras.persistence.Strip;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -23,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
  *
  * @author matheus
  */
-@ManagedBean(name="lasTirasList")
+@ManagedBean(name = "lasTirasList")
 @SessionScoped
 public class LasTirasList {
 
@@ -36,61 +37,137 @@ public class LasTirasList {
     private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     private String email;
     private String message;
-    private int counter;
+    //private int counter;
     private boolean visited = false;
-    private boolean tryToGo2morrow = false;
-    
+    private static final SimpleDateFormat sdfReqParameter = new SimpleDateFormat("yyyyMMdd");
+    private String currentDate;
     @EJB
     private VisitiHandlerLocal visitHandler;
-    
+
     public LasTirasList() {
-        
     }
 
-    public int getCounter() {
-        return counter;
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
     }
     
+    public String getLasTirasLink(){
+        return "http://www.kmtech.com.br:8080/LasTiras-web/faces/index.xhtml?q="+getCurrentFormattedDate();
+    }
     
+    public String getFacebookSrc(){
+        String src = "http://www.facebook.com/plugins/like.php?"
+                + "href= http%3A%2F%2Fwww.kmtech.com.br%3A8080%2FLasTiras-web%2Ffaces%2Findex.xhtml?q="+getCurrentFormattedDate()
+                + "&amp;layout=button_count"
+                + "&amp;show_faces=false"
+                + "&amp;action=recommended"
+                + "&amp;colorscheme=light"
+                + "&amp;width=175"
+                + "&amp;height=21"
+                + "&amp;font="
+                + "&amp;locale=pt_BR";
+        return src;
+    }
+    
+    private String getCurrentFormattedDate(){
+        LasTirasStrip lasStrip = getCurrentStripe();
+        Date date = lasStrip.getStripDate();
+        return sdfReqParameter.format(date);
+    }
 
-    public void setCounter(int counter) {
-        
-        if(counter>=0){
-            if(counter==1){
-                tryToGo2morrow = true;
-            }
-            this.counter = 0;
+    public String getNextDate() {
+        Date date = getParameterDate();
+        LasTirasStrip lasStrip = getCurrentStripe();
+        List<LasTirasStrip> newOnes = lasTirasHandler.getLasTirasNewerThenThis(lasStrip.getStripDate());
+        if(newOnes==null || newOnes.isEmpty()){
+            date = lasStrip.getStripDate();
         }
         else{
-            int maxCount = this.lasTirasHandler.getMaxLow();
-            if((counter)<-maxCount){
-                this.counter = -maxCount;
+            date = newOnes.get(0).getStripDate();
+        }
+        return sdfReqParameter.format(date);
+    }
+
+    public String getLastDate() {
+        Date date = getParameterDate();
+
+        LasTirasStrip lasStrip = getCurrentStripe();
+        
+        date = lasStrip.getStripDate();
+
+        Date dayBefore = new Date(date.getTime() - (1000 * 60 * 60 * 24));
+        return sdfReqParameter.format(dayBefore);
+    }
+
+//    public int getCounter() {
+//        return counter;
+//    }
+//    
+//    public void setCounter(int counter) {
+//        
+//        if(counter>=0){
+//            if(counter==1){
+//                tryToGo2morrow = true;
+//            }
+//            this.counter = 0;
+//        }
+//        else{
+//            int maxCount = this.lasTirasHandler.getMaxLow();
+//            if((counter)<-maxCount){
+//                this.counter = -maxCount;
+//            }
+//            else{
+//                this.counter = counter;                
+//            }
+//        }
+//    }
+    private String getPageParameter() {
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return req.getParameter("q");
+    }
+
+    private Date getParameterDate() {
+        try {
+            String dateString = getPageParameter();
+            if (dateString == null) {
+                return null;
             }
-            else{
-                this.counter = counter;                
-            }
+            return sdfReqParameter.parse(dateString);
+        } catch (Exception e) {
+            return null;
         }
     }
     
-    
+
     public LasTirasStrip getCurrentStripe() {
-        if(!visited){
-            HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();  
-            String ip = httpServletRequest.getRemoteAddr();  
+        if (!visited) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            String ip = httpServletRequest.getRemoteAddr();
             visitHandler.createVisit(ip);
             visited = true;
         }
+
         if (strip == null) {
-            if(counter>=0){
+            Date stripDate = getParameterDate();
+            if (stripDate == null) {
                 this.strip = this.lasTirasHandler.getNewerLasTiras();
-            }
-            else{
-                this.strip = this.lasTirasHandler.getIndexLasTiras(counter);
+            } else {
+                Date today = new Date();
+                if(stripDate.after(today)){
+                    this.strip = this.lasTirasHandler.getIndexLasTiras(today);                
+                }
+                else{
+                    this.strip = this.lasTirasHandler.getIndexLasTiras(stripDate);                
+                }
             }
         }
         return strip;
     }
-     
+
     public String getEmail() {
         return email;
     }
@@ -127,61 +204,33 @@ public class LasTirasList {
         logger.log(Level.INFO, "Date: " + date);
         return sdf.format(date);
     }
-    
-    public String acquireId(Strip stripSelected){
-        return "imageId"+stripSelected.getId();
+
+    public String acquireId(Strip stripSelected) {
+        return "imageId" + stripSelected.getId();
     }
-    
-    public String acquireJavaScritCall(Strip stripSelected){
+
+    public String acquireJavaScritCall(Strip stripSelected) {
         return "loadImage('" + stripSelected.getStripUrl() + "','" + acquireId(stripSelected) + "')";
     }
-        
-    public void cleanPage(){
+
+    public void cleanPage() {
         message = null;
         email = null;
         strip = null;
     }
-    
-    public String toLasTiras(){
+
+    public String toLasTiras() {
         cleanPage();
         return "index.xhtml";
     }
-    
-    public String toAuthors(){
+
+    public String toAuthors() {
         cleanPage();
-        return "authors.xhtml";        
+        return "authors.xhtml";
     }
-    
-     public String toContacts(){
+
+    public String toContacts() {
         cleanPage();
-        return "contato.xhtml";        
+        return "contato.xhtml";
     }
-     
-     public String toImageToNext(){
-         if(counter!=0){
-             return "resources/images/proximoOff.png";
-         }
-         else{
-             if(tryToGo2morrow){
-                return "resources/images/samanha.png";
-             }
-             else{
-                 return "resources/images/proximoOff.png";
-             }
-         }
-     }
-     
-     public String toImageToNextOn(){
-         if(counter!=0){
-             return "resources/images/proximoOn.png";
-         }
-         else{
-             if(tryToGo2morrow){
-                return "resources/images/samanha.png";
-             }
-             else{
-                 return "resources/images/proximoOn.png";
-             }
-         }
-     }
 }
